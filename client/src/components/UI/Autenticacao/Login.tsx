b74422ae6d73355b2c72e2edb2232f2d565719a3
Iagohss <iago.silva@ccc.ufcg.edu.br>
2024-04-11 09:03:39 -0300

    feat: implementação inicial da tela de login

--- client/src/components/UI/Autenticacao/Login.tsx
+++ client/src/components/UI/Autenticacao/Login.tsx
@@ -1,6 +1,34 @@
+import { Link, Navigate, useNavigate } from "react-router-dom"
+
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e : React.FormEvent) => {
+    e.preventDefault();
+    return navigate("/tarefas");
+  };
+
   return (
-    <div>Login</div>
+    <div className="login-form">
+        <h1>Realize seu Login</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Seu email:</label>
+            <input  
+                type="Text" 
+                id='email'
+                placeholder='Digite o seu email'
+            />
+            <label htmlFor="senha">Sua senha:</label>
+            <input  
+                type="password" 
+                id="senha"
+                placeholder='Digite a sua senha'
+            />
+            <button type='submit'>OK</button>
+        </form>
+        <Link className="link-cadastro" to="/cadastro"> Cadastrar-se </Link>
+    </div>
   )
 }
 
